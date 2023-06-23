49474a3faadea298ee4d61bf3655a1a52dddf4b5
Janaina Solidade <125398552+janasolidade@users.noreply.github.com>
2023-06-23 13:16:02 -0300

    Add files via upload

---
+++ javascript2/script.js
@@ -0,0 +1,23 @@
+function exibeAlerta(){
+    alert ("Página carregada com sucesso!")
+}
+
+exibeAlerta()
+
+var x = 50
+var y = 5
+
+document.write("A soma de x + y é "  ,x + y, "<br><br>");
+document.write("A divisão de x + y é "  ,x / y, "<br><br>");
+document.write("A multiplicação de x + y é "  ,x * y, "<br><br>");
+
+
+
+ var firstNumber = 250
+ var secondNumber = 350
+
+ console.log(firstNumber===secondNumber)
+   
+
+
+
